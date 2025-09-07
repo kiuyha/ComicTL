0ba81fcf9bcbb37fc103d309e3b9cdb3f9ac6c18
kiuyha <ketutshridhara@gmail.com>
2025-09-07 23:57:41 +0700

    initial commit

---
+++ wxt.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  manifest: {
+    name: "Kalashi",
+  },
+  srcDir: "src",
+  modules: ["@wxt-dev/module-svelte"],
+  webExt: {
+    binaries: {
+      firefox: "/usr/bin/firefox",
+    },
+  },
+});
